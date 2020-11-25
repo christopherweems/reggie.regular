cb43c232304d864f40db6d246c4c7df5f0c63b15
Christopher Weems <hello@christopherweems.com>
2020-11-25 15:13:10 -0500

    Update package dependency requirements

--- Package.swift
+++ Package.swift
@@ -19,7 +19,7 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/christopherweems/unstandard.git", .branch("main")),
+        .package(url: "https://github.com/christopherweems/unstandard.git", .upToNextMajor(from: "0.0.6")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
